57ea2b78735ca77df1146809cd873e2280d935af
teruma-nanami <kinos.travel.fun@gmail.com>
2024-05-26 18:17:27 +0900

    make blade

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  @yield('css')
+  <title>Contact Form</title>
+</head>
+<body>
+  <main>
+    @yield('content')
+  </main>
+</body>
+</html>
